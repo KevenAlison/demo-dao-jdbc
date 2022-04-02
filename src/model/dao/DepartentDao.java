4977897cd2dd4d47d1e0aa782b1c4506b7971b4c
Keven Alison <kevenalison13@gmail.com>
2022-04-02 03:00:32 -0300

    Interfaces de departamentos e vendedores

---
+++ src/model/dao/DepartentDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartentDao {
+	
+	void insert(Department department);
+	void upadte(Department department);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+	
+}

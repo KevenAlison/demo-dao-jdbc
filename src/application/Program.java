3a4398e58ff0f058e9aca42529dff8865153cdd3
Keven Alison <kevenalison13@gmail.com>
2022-04-02 02:31:11 -0300

    Classe departamento

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+	public static void main(String[] args) {
+	Department departamento = new Department(1, "Roupas");
+	System.out.println(departamento);
+	}
+}

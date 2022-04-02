e4cd5f6c117bf2511cf4cf347d8cd682c6fbf729
Keven Alison <kevenalison13@gmail.com>
2022-04-02 02:41:03 -0300

    Classe vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 	Department departamento = new Department(1, "Roupas");
+	Seller seller = new Seller(1, "Keven", "keveenalison@gmail.com",new Date(), 1000.0, departamento);
+	
 	System.out.println(departamento);
+	System.out.println(seller);
 	}
 }

a84caad098cad059f4b557b9ad38f1a43e250823
Keven Alison <kevenalison13@gmail.com>
2022-04-02 21:01:23 -0300

    Implementando delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,6 +11,8 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -38,14 +41,23 @@ public class Program {
 		
 		System.out.println("==== TEST 4: seeller insert ====");
 		Seller newSeller = new Seller(null, "Kymi", "kymiandersoon@gmail.com", new Date(), 2000.0, dep);
-		sellerDao.insert(newSeller);
+		//sellerDao.insert(newSeller);
 		System.out.println("Seller insrted!");
 		
-		System.out.println("==== TEST 4: seeller update ====");
+		System.out.println("==== TEST 5: seeller update ====");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.upadte(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("==== TEST 6: seeller update ====");
+		System.out.println("Entre com o ID para deletar um vendedor");
+		int idseller = sc.nextInt();
+		sellerDao.deleteById(idseller);
+		System.out.println("Deleção completa");
+		
+		sc.close();
+
 		
 
 	}
